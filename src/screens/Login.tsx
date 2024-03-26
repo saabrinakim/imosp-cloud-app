7d371dc0570e5f267fc8ef94750cc4f582f2d249
Flávio Lee <>
2024-03-26 09:38:13 -0300

    deleted Login function on Login screen; altered function DisplayImage to Login; add Image to login screen; add style to texts; add button backButton to Login screen; altered styleSheets components;

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -6,18 +6,7 @@ type Props = {
     navigation: any;
   };
 
- function Login(props: Props) {
-    return(
-        <SafeAreaView style={styles.container}>
-            <Text variant="displayMedium">Login</Text>
-            <Text onPress={() => props.navigation.goBack()}> Click me to go back </Text>
-            
-        </SafeAreaView> 
-        
-    );  
- }
-
- const DisplayAnImage = () => {
+ const Login = (props: Props) => {
   const [text, onChangeText] = React.useState("");
   return (
    
@@ -27,22 +16,41 @@ type Props = {
     keyboardVerticalOffset= {Platform.OS === "ios" ? 100 : 0} 
     style={styles.container}
     >
+      <Image source={require("../../assets/imosplogoprovisorio.png")} style={styles.logo} resizeMode="cover"/>
+      <Text style={{
+        fontSize: 22, 
+        fontWeight: 'bold', 
+        marginBottom: 95
+        }}>IMOSP cloud</Text>
       
-      <Text>Digite o seu email!</Text>
+      
+      <Text style={{
+        fontSize: 16, 
+        alignSelf: 'left', 
+        color: "#1A73E8", 
+        marginHorizontal: 42
+        }}>Digite seu email</Text>
    
       <TextInput style={styles.input}
         placeholder= "Escreva o seu email"
         onChangeText={onChangeText}
         value={text}
       ></TextInput>
+
       <TextInput style={styles.input}
         placeholder= "Digite a sua senha"
         onChangeText={onChangeText}
         value={text}
       ></TextInput>
+
       <TouchableOpacity style={styles.button}>
-      <Text>Login</Text>
+        <Text style={{fontSize: 16}}>Entrar</Text>
       </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.backButton} onPress={() => props.navigation.goBack()}> 
+        <Text style={{fontSize: 16}}>Voltar</Text> 
+        {/*apenas um botão provisório pra voltar a página pra matar a agonia huehue*/}
+        </TouchableOpacity>
       </KeyboardAvoidingView>
   );
 };
@@ -50,27 +58,49 @@ type Props = {
  
  const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      marginHorizontal: 24,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  logo:{
+    marginBottom: 20,
+    width: 102,
+    height: 89,
+    marginTop: 150,
+
   },
   button: {
     marginTop: 12,
     backgroundColor: "#F4B325",
-    width: "30%",
+    width: "80%",
     alignItems: 'center',
     borderRadius: 8,
     paddingVertical: 12,
+    justifyContent: 'center',
+    marginHorizontal: 24
+    
   },
   input: {
-  marginTop: 12,
-  backgroundColor: "#ffffff",
-  width: "50%",
-  alignItems: 'center',
-  borderRadius: 8,
-  paddingVertical: 12,
-  },
-  
+    marginTop: 9,
+    backgroundColor: "#fff",
+    width: "80%",
+    alignItems: 'center',
+    paddingVertical: 12,
+    justifyContent: 'center',
+    borderWidth: 1,
+    marginHorizontal: 24
 
+  },
+  backButton: {
+    marginTop: 4,
+    backgroundColor: "#F4B325",
+    width: "80%",
+    alignItems: 'center',
+    borderRadius: 8,
+    paddingVertical: 12,
+    justifyContent: 'center'
+  }
   }
  )
  
