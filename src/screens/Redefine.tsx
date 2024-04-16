87f88fd84fb38c0b42a7aceef0b36468411d615a
LIPE-DESKTOP\WINDOWS <filipevallemoreira@hotmail.com>
2024-04-16 18:06:00 -0300

    Add Inputs to Redefine

--- src/screens/Redefine.tsx
+++ src/screens/Redefine.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View, Image, TouchableOpacity, TextInput, KeyboardAvoidingView, Keyboard,} from "react-native";
 import { Text } from "react-native-paper";
 
 type Props = {
@@ -9,7 +9,16 @@ type Props = {
 function Redefine(props: Props) {
   return (
     <View style={styles.container}>
+      <Text style={styles.comando}>Digite o código de verificação</Text>
 
+      <Text style ={styles.info}> O código de verificação foi enviado para somelongusername@email.com</Text>
+      
+      <View style={styles.row}>
+      <TextInput style={styles.inputP}></TextInput>
+      <TextInput style={styles.inputP}></TextInput>
+      <TextInput style={styles.inputP}></TextInput>
+      <TextInput style={styles.inputP}></TextInput>
+      </View>
     </View>
   );
 }
@@ -17,8 +26,40 @@ function Redefine(props: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 24,
+    marginTop: 20,
   },
+  comando: {
+    width: 321,
+    alignItems: 'center',
+    textAlign: 'center',
+    top: 80,
+    left: 34,
+    fontSize: 22,
+    lineHeight: 26.63,
+  },
+  info: {
+    width: 305,
+    top: 90,
+    left: 44,
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+
+  },
+  inputP: {
+    display: 'flex',
+    left: 77,
+    width: 46.99,
+    height: 44,
+    top: 110,
+    marginTop: 20,
+    marginEnd: 20,
+    padding: 10,
+    borderWidth: 1,
+  },
+  row: {
+    flexDirection: 'row',
+  }
 });
 
 export default Redefine;
