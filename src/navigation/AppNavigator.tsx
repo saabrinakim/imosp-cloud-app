0406e7f8329b7beb0eeb250ddaa9416539b62569
Sabrina Kim <saabrinakim@gmail.com>
2024-08-10 00:23:28 -0300

    Add finished home screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import Feather from "@expo/vector-icons/Feather";
 
 import Home from "../screens/Home";
 import About from "../screens/About";
@@ -11,6 +13,66 @@ import Events from "../screens/Events";
 import Service from "../screens/Service";
 
 const AppStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#2C2C2C",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Agenda"
+        component={Events}
+        options={{
+          tabBarLabel: "Agenda",
+          tabBarIcon: ({ color }) => (
+            <Feather name="calendar" size={24} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Ministérios"
+        component={Ministries}
+        options={{
+          tabBarLabel: "Ministérios",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Sobre"
+        component={About}
+        options={{
+          tabBarLabel: "Sobre",
+          tabBarIcon: ({ color }) => (
+            <Feather name="bookmark" size={24} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 function AppNavigator() {
   return (
@@ -20,13 +82,10 @@ function AppNavigator() {
         headerShown: false,
       }}
     >
+      <AppStack.Screen name="Home" component={HomeTabs} />
       <AppStack.Screen name="Onboarding" component={Onboarding} />
-      <AppStack.Screen name="Home" component={Home} />
-      <AppStack.Screen name="About" component={About} />
       <AppStack.Screen name="Cadastro" component={Cadastro} />
       <AppStack.Screen name="Login" component={Login} />
-      <AppStack.Screen name="Ministries" component={Ministries} />
-      <AppStack.Screen name="Events" component={Events} />
       <AppStack.Screen name="Service" component={Service} />
     </AppStack.Navigator>
   );
