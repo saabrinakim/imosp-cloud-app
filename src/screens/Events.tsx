e47c2d3c96b76ad7d29ecf189136cc82c79e7a62
LIPE-DESKTOP\WINDOWS <filipevallemoreira@hotmail.com>
2024-08-21 13:47:47 -0300

    Add event box

--- src/screens/Events.tsx
+++ src/screens/Events.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import BackButton from "../components/BackButton";
+import { Button } from "react-native-paper";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Feather from '@expo/vector-icons/Feather';
 
 type Props = {
   navigation: any;
@@ -13,6 +16,71 @@ function Events(props: Props) {
         <BackButton navigation={props.navigation} styles={styles.backButton} />
         <Text style={styles.title}>Eventos</Text>
       </View>
+      
+      <View style={{
+        width: 328, 
+        height: 188, 
+        backgroundColor: '#FFFFFF', 
+        alignSelf: 'center', 
+        alignItems: 'center',
+        }}>
+        
+        <View style={{width: 304, height: 89, marginTop: 8,}}>
+          <View style={{flexDirection: "row"}}>
+
+            <Image style={styles.image} source={require('../../assets/retirotabernaculo.png')}
+            />
+
+            <View style={{marginLeft: 4}}>
+            <Text style={styles.eventTitle}>O Tabernáculo e a Espiritualidade Cristã</Text>
+            <Text style={styles.description}>Inscrições abertas até o dia 20/04</Text>
+            </View>
+            
+          </View>
+
+          <View style={styles.infoBar}>
+            <View style={{flexDirection:"row", alignItems: 'center'}}>
+
+              <Feather name="calendar" size={16} color="black" />
+
+              <Text style={styles.infoText}>22-26/07/24</Text>
+
+            </View>
+
+            <View style={{flexDirection:"row", alignItems: 'center', marginLeft: 8}}>
+
+              <Feather name="map-pin" size={15.88} color="black" />
+
+              <Text style={styles.infoText}>Chácara retiro</Text>
+
+              <View style={styles.share}>
+
+                <Feather name="share-2" size={16.67} color="black"/>
+
+              </View>
+
+            </View>
+
+          </View>
+
+          <TouchableOpacity style={styles.button}>
+            <View style={{flexDirection: "row"}}>
+              
+              <View style={styles.plus}>
+              
+                <Feather name="plus" size={20} color="#1A73E8"/> 
+
+              </View>
+                
+              <Text style={styles.buttonText}>Inscrições</Text>
+
+            </View>
+
+          </TouchableOpacity>
+          
+        </View>
+      </View>
+      
     </SafeAreaView>
   );
 }
@@ -39,6 +107,62 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
   },
+  eventTitle: {
+    fontSize:  16,
+    lineHeight: 19.2,
+    width: 203,
+    height: 39,
+    letterSpacing: 0.5,
+    color: '#333333'
+  },
+  description: {
+    fontSize: 14,
+    lineHeight: 16.8,
+    width: 203,
+    height: 34,
+    letterSpacing: 0.5,
+    color: '#757575'
+  },
+  image: {
+    width: 93,
+    height: 89,
+    borderRadius: 8,
+  },
+  infoBar: {
+    width: 304,
+    height: 20,
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+  infoText: {
+    fontSize: 12,
+    color: '#1E1E1E',
+    marginLeft: 4,
+  },
+  button: {
+    width: 304,
+    height: 39,
+    borderRadius: 8,
+    backgroundColor: '#1A73E833',
+    alignItems: 'center',
+    marginTop: 12,  
+  },
+  buttonText: {
+    fontSize:  16,
+    color: '#1A73E8',
+    height: 23,
+    textAlignVertical: "center",
+    marginTop: 5,
+  },
+  share: {
+    marginLeft: 90,
+  },
+  plus: {
+    marginTop: 7,
+    marginRight: 10,
+
+  },
+
 });
 
 export default Events;
